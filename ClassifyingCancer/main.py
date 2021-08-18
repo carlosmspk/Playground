3b241176321dcbf8c0403d10c6ecf65767b5acca
carlosmspk <carlos_cp_santos@hotmail.com>
2021-08-18 21:59:57 +0100

    short description

--- ClassifyingCancer/main.py
+++ ClassifyingCancer/main.py
@@ -1,1 +1,6 @@
+# THIS IS NOT YET COMPLETE
+
+# These scripts assumes you have a folder called "datasets" in the same directory as this script.
+# Invasive Ductal Carcinoma (IDC) Histology Image Dataset is the dataset being used.
+
 print("Hello World!")  
